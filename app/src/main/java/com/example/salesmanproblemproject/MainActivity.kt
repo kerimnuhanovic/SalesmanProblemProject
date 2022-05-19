8867d24de79a9cd235f9419ca3e0b5ec0e63bc38
kerimnuhanovic <kerimnuhanovic18@gmail.com>
2022-05-19 23:24:36 +0200

    Update1

--- app/src/main/java/com/example/salesmanproblemproject/MainActivity.kt
+++ app/src/main/java/com/example/salesmanproblemproject/MainActivity.kt
@@ -2,16 +2,13 @@ package com.example.salesmanproblemproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.salesmanproblemproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        print("Kerim")
-        print("Kerim ponovo ide frizbi")
-        print("Sabah")
-        print("Kerim poslije sabaha!")
-        print("Tala poslije sabaha")
-        print("Samjke 22 boda")
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
